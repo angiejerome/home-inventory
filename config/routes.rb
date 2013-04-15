0d87f483fba2cb860ae79e5df3f5f6caf497979e
Steve Tuckner <steve@open-orgsolutions.com>
2013-04-14 22:26:37 -0500

    made inventory_items#index the default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 HomeInventory::Application.routes.draw do
   resources :inventory_items
 
+  root :to => "inventory_items#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
